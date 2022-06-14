e3a8af3026603f51e17a56a37be1652fbde70c89
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-06-13 22:44:09 -0500

    Actualizar personal data module

--- src/personal_data/personal_data.module.ts
+++ src/personal_data/personal_data.module.ts
@@ -1,8 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { PersonalDataService } from './personal_data.service';
 import { PersonalDataController } from './personal_data.controller';
+import { PersonalDataEntity } from './personal_data.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PersonalDataEntity])],
   providers: [PersonalDataService],
   controllers: [PersonalDataController]
 })
