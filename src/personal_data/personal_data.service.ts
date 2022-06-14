a2c36840955a13ecf77309cfd2e3311e3d5e94fb
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-06-13 22:44:37 -0500

    Ajustes inicial del servicio de personal data

--- src/personal_data/personal_data.service.ts
+++ src/personal_data/personal_data.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { PersonalDataRepository } from './personal_data.repository';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PersonalDataEntity } from './personal_data.entity';
 
 @Injectable()
-export class PersonalDataService {}
+export class PersonalDataService {
+
+    constructor(
+        @InjectRepository(PersonalDataRepository) 
+        private personalDataRepository: PersonalDataRepository
+        ) { }
+
+    async getAll(): Promise<PersonalDataEntity[]>{
+        const list = await this.personalDataRepository.find();
+        if(!list.length){
+            throw new NotFoundException({ message: 'la lista está vacía'});
+        }
+        return list;
+    }
+}
