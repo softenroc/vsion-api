bbccb7d10458a3019b22ee5777aa3ef1bc2b45b5
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-06-13 22:08:10 -0500

    Adicionar conexion a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from './config/constants';
+import { dirname } from 'path';
+
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: '.env',
     isGlobal: true
+  }),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      type: 'mysql',
+      host: configService.get<string>(DB_HOST),
+      port: +configService.get<string>(DB_PORT),
+      username: configService.get<string>(DB_USER),
+      password: configService.get<string>(DB_PASSWORD),
+      database: configService.get<string>(DB_DATABASE),
+      entities: [ dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    inject: [ConfigService],
   })],
   controllers: [AppController],
   providers: [AppService],
