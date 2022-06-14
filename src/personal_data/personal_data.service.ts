7a47cf270f882d679a77d7c61e7f1c8a7cc696e0
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-06-13 23:32:03 -0500

    Adicionar metodos a personal data service

--- src/personal_data/personal_data.service.ts
+++ src/personal_data/personal_data.service.ts
@@ -1,10 +1,14 @@
+import { PersonalDataDto } from './dto/personal_data.dto';
 import { PersonalDataRepository } from './personal_data.repository';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PersonalDataEntity } from './personal_data.entity';
+import { PersonalDataMapper } from './dto/personal_data.mapper';
 
 @Injectable()
 export class PersonalDataService {
+
+    personalDataMapper: PersonalDataMapper;
 
     constructor(
         @InjectRepository(PersonalDataRepository) 
@@ -17,5 +21,43 @@ export class PersonalDataService {
             throw new NotFoundException({ message: 'la lista está vacía'});
         }
         return list;
+    }
+
+    async findById(id: number): Promise<PersonalDataEntity> {
+        const personalData = await this.personalDataRepository.findOneBy({id: id});
+        if(!personalData){
+            throw new NotFoundException({message: 'no existe el paciente'});
+        }
+
+        return personalData;
+    }
+
+    async findByDocument(document: string): Promise<PersonalDataEntity> {
+        const personalData = await this.personalDataRepository.findOneBy({document: document});
+        if(!personalData){
+            throw new NotFoundException({message: 'no existe el paciente'});
+        }
+
+        return personalData;
+    }
+
+    async create(dto: PersonalDataDto): Promise<any> {
+        const personalData = this.personalDataRepository.create(dto);
+        await this.personalDataRepository.save(personalData);
+        return {message: `paciente ${personalData.name} registrado correctamente`};
+    }
+
+    
+    async update(document: string, dto: PersonalDataDto): Promise<any> {
+        let personalData = await this.personalDataRepository.findOneBy({document: document});
+        personalData =  this.personalDataMapper.mapper(dto);
+        await this.personalDataRepository.save(personalData);
+        return {message: 'informacion del paciente actualizada correctamente'};
+    }
+
+    async delete(document: string): Promise<any> {
+        const personalData = await this.personalDataRepository.findOneBy({document: document});        
+        await this.personalDataRepository.delete(personalData);
+        return {message: `paciente  ${personalData.name} eliminado`};
     }
 }
