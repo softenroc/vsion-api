5316a832b1bb6a6b361867f4b735e31c84e7a974
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-09-18 15:31:37 -0500

    adicionar controlador para factura

--- src/sale_data/sale_data.controller.ts
+++ src/sale_data/sale_data.controller.ts
@@ -5,6 +5,40 @@ import { Body, Controller, Delete, Get, Param, Post, Put, UsePipes, ValidationPi
 @Controller('sale-data')
 export class SaleDataController {
 
-    constructor(private readonly saleDataService: SaleDataService) {}
+    constructor(private readonly saleDataService: SaleDataService) {}    
+
+    @Get()
+    async getAll(){
+        console.log(`getAll sale-data`);
+        return await this.saleDataService.getAll();
+    }
+
+    @Get('by_document/:document')
+    async getOneByDocyment(@Param('document') document: string){
+
+        return this.saleDataService.findByDocument(document);
+    }
+
+    @Get('by_invoice/:invoice_id')
+    async getOneByInvoiceId(@Param('invoice_id') invoice_id: string){
+
+        return this.saleDataService.findByInvoice(invoice_id);
+    }
+
+    @UsePipes(new ValidationPipe())
+    @Post()
+    async create(@Body() dto: SaleDataDto){
+        return this.saleDataService.create(dto);
+    }
+
+    @Put()
+    async update(@Param('invoice_id') invoice_id: string, @Body() dto: SaleDataDto){
+        return this.saleDataService.update(invoice_id, dto);
+    }
+
+    @Delete(':invoice_id')
+    async delete(@Param('invoice_id') invoice_id: string){
+        return this.saleDataService.delete(invoice_id);
+    }
 
 }
